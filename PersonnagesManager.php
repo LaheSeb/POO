01b2feaa640bbc4b4d755f7b52cff2300d60feb3
LAHEYNE Sebastien <laheseb@ndlp.fr>
2021-09-23 12:01:53 +0200

    manager

--- PersonnagesManager.php
+++ PersonnagesManager.php
@@ -19,7 +19,21 @@ class PersonnagesManager
     public function getOne(){
 
     }
-    public function getList(){
+    public function getList():array{
+
+        $listeDePersonnage = array();
+        // retourne la liste de tous les perosnnages 
+        $request = $this->_db->query('SELECT id, nom, `force`, degats, niveau, experience FROM personnages; ');
+
+        while ($ligne = $request->fetch(PDO::FETCH_ASSOC))// Chauqe entrée seta recuoerer
+        {
+            // On passe les donnés (stock"s dans un tableau) concernant le personnae
+
+            $perso= new Personnage($ligne);
+            $listeDePersonnage[] = $perso; // Ajouter personnage au tableau 
+          
+        }
+        return $listeDePersonnage;
 
     }
     
--- index1.php
+++ index1.php
@@ -9,8 +9,18 @@ include "conf.php";
 
 try {
     $db = new PDO ($dsn, $user, $password);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
 
     $personnagesManager = new PersonnagesManager($db);
+   $personnages = $personnagesManager->getList();
+    print('<br>Liste des personnages');
+   foreach ($personnages as $personnage)
+   {
+       print('<br> ' . $personnage->getNom());
+   
+
+   }
    /* $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
     if($db){
